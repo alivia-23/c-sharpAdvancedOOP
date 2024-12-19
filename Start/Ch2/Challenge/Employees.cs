b8837be4292e8d90c6d4631df01562cf705588c1
alivia-23 <guin.al@husky.neu.edu>
2024-12-19 12:55:48 -0800

    challenge exercise

--- Start/Ch2/Challenge/Employees.cs
+++ Start/Ch2/Challenge/Employees.cs
@@ -1,12 +1,35 @@
 // Example file for Advanced C#: Object Oriented Programming by Joe Marini
 // Solution to the Employee Class challenge
 
+using System.Diagnostics.CodeAnalysis;
 public class Employee {
     public Employee() {}
 
-    // YOUR CODE GOES HERE
+    public required int ID { get; init; }
+    public required string Department { get; set; }
+
+    public required string FullName { get; set; }
+    public virtual void AdjustPay(decimal percent) {} 
 
     public override string ToString() => $"{ID}:{FullName}, {Department} ";
 }
 
 // DEFINE OTHER CLASSES HERE
+public class HourlyEmployee : Employee {
+    public decimal PayRate { get; set; }
+
+    public HourlyEmployee() {}
+    public override void AdjustPay(decimal percent) {
+        PayRate += (PayRate * percent);
+    }
+}
+
+public class SalariedEmployee : Employee {
+    public decimal Salary { get; set; }
+    public SalariedEmployee() {}
+
+    public override void AdjustPay(decimal percent) {
+        Salary += (Salary * percent);
+    }
+    
+}
--- Start/Ch2/Challenge/obj/Debug/net7.0/Challenge.AssemblyInfo.cs
+++ Start/Ch2/Challenge/obj/Debug/net7.0/Challenge.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Challenge")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8d287d48783d7448dd2ceb4c8c6ce7b38f648ba7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+164960691e44ec202dfc1973c6edf2b5c72a1d5d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Challenge")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Challenge")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
