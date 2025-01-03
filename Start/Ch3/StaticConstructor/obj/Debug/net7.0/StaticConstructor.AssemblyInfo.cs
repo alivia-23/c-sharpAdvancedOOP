6c7a9a4d9f6c27ec7cffbfbf7df889c37a90a8e9
alivia-23 <guin.al@husky.neu.edu>
2025-01-03 12:50:14 -0800

    static class

--- Start/Ch3/StaticConstructor/obj/Debug/net7.0/StaticConstructor.AssemblyInfo.cs
+++ Start/Ch3/StaticConstructor/obj/Debug/net7.0/StaticConstructor.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("StaticConstructor")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e80d9ce1c7462c447f2ab9a5dc968b2e43d0aba9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d4a665f831871fb6b35351b05b7f84618a8a3aee")]
 [assembly: System.Reflection.AssemblyProductAttribute("StaticConstructor")]
 [assembly: System.Reflection.AssemblyTitleAttribute("StaticConstructor")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
